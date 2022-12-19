d3c8ef3d84cb9526497670e25577d6184321708d
diegotorreslopez81 <diego@infinitelabs.co>
2022-12-19 23:40:02 +0100

    wallet connection added

--- my-app_/src/app/app.component.ts
+++ my-app_/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ethers } from 'ethers';
+import { WalletService } from './services/wallet.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,11 @@ export class AppComponent {
   submitAnswer() {
   throw new Error('Method not implemented.');
 }
+  public walletConnected: boolean = false;
+  public walletId: string = '';
+  provider: ethers.providers.Provider;
 
-  title = 'my-app';
+  title = 'Proof your Knowledge and get an exclusive NFT';
   number = 44;
   score= 0;
   answers: string[] | undefined;
@@ -21,11 +26,31 @@ export class AppComponent {
   question: number | undefined ;
 
 
-  constructor() {
+  constructor(private walletService: WalletService) {
+    this.provider = ethers.providers.getDefaultProvider("goerli");
+    this.connectToWallet  = async () => {
+      const accounts = await this.walletService.connectWallet();
+      if(accounts.length > 0){
+        this.walletConnected = true;
+        this.walletId = accounts[0];
+      }
+    }
     this.currentQuestion = 0;
     this.totalQuestions = 0;
     this.userAnswer = 0;
     this.answers = ["Option 1","Option 2","Option 3","Option 4"];
   }
+  connectToWallet  = async () => {
 
+  }  
+  checkWalletConnected = async () => {
+    const accounts = await this.walletService.checkWalletConnected();
+    if(accounts.length > 0){
+      this.walletConnected = true;
+      this.walletId = accounts[0];
+    }
+  }
+  ngOnInit(): void {
+    this.checkWalletConnected();
+  }
 }
