47d4c64cc0b9950793bbc4d1c9b84d5078145e11
diegotorreslopez81 <diego@infinitelabs.co>
2022-12-19 22:22:31 +0100

    fix quiz

--- my-app_/src/app/app.component.ts
+++ my-app_/src/app/app.component.ts
@@ -6,13 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  submitAnswer() {
+  throw new Error('Method not implemented.');
+}
+
   title = 'my-app';
   number = 44;
   score= 0;
-  answers: number | undefined;
+  answers: string[] | undefined;
   totalQuestions: number | undefined;
   currentQuestion: number | undefined;
   userAnswer: number | undefined;
   question: number | undefined ;
+
+
+  constructor() {
+    this.currentQuestion = 0;
+    this.totalQuestions = 0;
+    this.userAnswer = 0;
+    this.answers = ["Option 1","Option 2","Option 3","Option 4"];
+  }
 
 }
